25d1ce7dd07fe05dc318d9041a030e8532ffb9d5
jerryfangr <2a15068517423@gmail.com>
2021-03-21 19:28:28 +0800

    config compatibilty

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,12 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
+import './style/normalize.scss';
+import './style/common.scss';
 import HelloWorld from './components/HelloWorld.vue'
 
 export default {
